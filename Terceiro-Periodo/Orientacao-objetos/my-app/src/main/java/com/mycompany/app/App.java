564b1d3c51fc594ff80d21cf158d12bf5fd8a4bd
Matheus <108198029+mmsantos147@users.noreply.github.com>
2023-03-31 14:23:31 -0300

    Adição do terceiro periodo
    Codigos do terceiro periodo da faculdade

---
+++ Terceiro-Periodo/Orientacao-objetos/my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int x = 20, y = 15, z = x + y;
+        System.out.println(z);
+    }
+}
---
+++ Terceiro-Periodo/Orientacao-objetos/teste/src/teste.java
@@ -0,0 +1,19 @@
+public class teste {
+	
+	public static void main(String[] args) {
+		int[] n = {0,0,0,0,0,0,0,0,0,0};
+		int[] x = {0,0,0,0,0};
+		int[] y = {0,0,0,0,0};
+			for (int i = 0; i < 10; i++) {
+				n[i] = (int)(Math.random() * 101);
+				System.out.println(n[i]);
+			}
+			for (int i = 0, j = 0; i< 5 || j < 10; i++, j = j+2) {
+				x[i] = Math.max(n[j], n[j+1]);
+				y[i] = Math.min(n[j], n[j+1]);
+				System.out.println(x[i]);
+				System.out.println(y[i]);
+			}
+		}
+	}
+	
